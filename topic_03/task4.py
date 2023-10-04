fea20f3607dcef0a6ca06acf38e0c664062e7190
unknown <bohbohg41@gmail.com>
2023-10-04 21:05:04 +0300

    123

--- topic_03/task4.py
+++ topic_03/task4.py
@@ -1,8 +1,11 @@
+def PosSearch(a,b):
+    for i in range(len(a)):
+        if a[i] == b:
+            return i
 a = ["aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", "ii", "jj", "kk", "ll", "mm", "nn", "oo", "pp", "qq", "rr", "ss", "tt", "uu", "vv", "ww", "xx", "yy", "zz"]
 b = str(input("b: "))
 a.append(b)
 a.sort()
 print (a)
-for i in range(len(a)):
-    if a[i] == b:
-        print ("Елемент ", b," знаходиться на ",i," позиції")
+i = PosSearch (a,b)
+print ("Елемент ", b," знаходиться на ",i," позиції")
